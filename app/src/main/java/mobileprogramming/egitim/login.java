34eee66642d61320e39d87531d332bd8caa7d46f
saidkartal <as_kartal@hotmail.com>
2018-04-11 14:51:29 +0300

    Login,register bağlantıları kuruldu

--- app/src/main/java/mobileprogramming/egitim/login.java
+++ app/src/main/java/mobileprogramming/egitim/login.java
@@ -1,13 +1,26 @@
 package mobileprogramming.egitim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
-
+    TextView KayıtOl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        KayıtOl=(TextView)findViewById(R.id.textView5);
+        KayıtOl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(login.this,register.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/mobileprogramming/egitim/register.java
+++ app/src/main/java/mobileprogramming/egitim/register.java
@@ -1,13 +1,24 @@
 package mobileprogramming.egitim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class register extends AppCompatActivity {
-
+    TextView girissayfası;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        girissayfası=(TextView)findViewById(R.id.textView8);
+        girissayfası.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(register.this,login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
