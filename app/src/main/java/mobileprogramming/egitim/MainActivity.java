06ac44569094dc269b533496097501bb108081a6
saidkartal <as_kartal@hotmail.com>
2018-04-11 14:02:07 +0300

    Studio to github

---
+++ app/src/main/java/mobileprogramming/egitim/MainActivity.java
@@ -0,0 +1,74 @@
+package mobileprogramming.egitim;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar mProgressBar;
+    private TextView mLoadingText;
+    private int mProgressStatus = 0;
+
+    private Handler mHandler=new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mProgressBar=(ProgressBar)findViewById(R.id.progressbar);
+        mLoadingText=(TextView)findViewById(R.id.LoadingCompleteTextView);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mProgressStatus<100){
+                    mProgressStatus++;
+                    android.os.SystemClock.sleep(50);
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mProgressBar.setProgress(mProgressStatus);
+
+                        }
+                    });
+                }
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mLoadingText.setVisibility(View.VISIBLE);
+                    }
+                });
+            }
+        }).start();
+
+
+
+
+        Thread thread=new Thread(){
+
+            @Override
+            public void run()
+            {
+                try{
+                    sleep(5000);
+                    Intent intent=new Intent(getApplicationContext(),login.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+
+                }
+            }
+        };
+        thread.start();
+    }
+
+
+}
