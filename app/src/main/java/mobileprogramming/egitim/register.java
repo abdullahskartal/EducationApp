9ceb7b9accd0caf4ca96294fb2e5eee1769f03e9
saidkartal <as_kartal@hotmail.com>
2018-04-13 15:29:42 +0300

    Register sayfası oluşturuldu.

--- app/src/main/java/mobileprogramming/egitim/register.java
+++ app/src/main/java/mobileprogramming/egitim/register.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,15 +15,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.w3c.dom.Text;
 
 
-public class register extends AppCompatActivity implements View.OnClickListener {
+public class register extends AppCompatActivity {
     private EditText register_email,register_sifre;
     private FirebaseAuth auth;
-    TextView girissayfası,hesapolustur;
-    EditText email,sifre;
+    EditText email, sifre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,70 +31,32 @@ public class register extends AppCompatActivity implements View.OnClickListener 
 
         auth=FirebaseAuth.getInstance();
 
-        email=(EditText)findViewById(R.id.register_email);
-        sifre=(EditText)findViewById(R.id.register_sifre);
-        hesapolustur=(TextView)findViewById(R.id.hesap_olustur);
+        this.email=findViewById(R.id.register_email);
+        this.sifre=findViewById(R.id.register_sifre);
 
-        hesapolustur.setOnClickListener(this);
-
-
-        hesapolustur.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email=register_email.getText().toString().trim();
-                String sifre=register_sifre.getText().toString().trim();
-
-
-
-
-                if (TextUtils.isEmpty(email)){
-                    Toast.makeText(getApplicationContext(),"Email adresinizi giriniz!",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(sifre)){
-                    Toast.makeText(getApplicationContext(),"Şifrenizi giriniz!",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (sifre.length()<6){
-                    Toast.makeText(getApplicationContext(),"Şifreniz minimum 6 karakter barındırmalı!",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                auth.createUserWithEmailAndPassword(email,sifre)
-                        .addOnCompleteListener(register.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                Toast.makeText(register.this,"createUserWithEmail:onComplete:"+task.isSuccessful(),Toast.LENGTH_SHORT).show();
-
-                                if (!task.isSuccessful()){
-                                    Toast.makeText(register.this,"Yetkilendirme hatası"+task.getException(),Toast.LENGTH_SHORT).show();
-                                }
-                                else {
-                                    startActivity(new Intent(register.this,login.class));
-                                    finish();
-                                }
-                            }
-                        });
-
-            }
-        });
-
-
-        girissayfası=(TextView)findViewById(R.id.textView8);
-        girissayfası.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(register.this,login.class);
-                startActivity(intent);
-            }
-        });
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.hesap_olustur:
-                break;
-        }
+    public void registerButton (View view) {
+        String rEmail = this.email.getText().toString();
+        String rPassword = this.sifre.getText().toString();
+        auth.createUserWithEmailAndPassword(rEmail, rPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(register.this, "Authentication completed.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(register.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
+    public void startLoginActivity (View view) {
+        Intent startActivity = new Intent(register.this, login.class);
+        startActivity(startActivity);
+    }
+
 }
