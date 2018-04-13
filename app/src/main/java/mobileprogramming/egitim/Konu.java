c9de55b3d9de8f2de93d5f52cbcd322e4ff7b344
Savaş Arvas <savasarvas@gmail.com>
2018-04-13 15:35:37 +0300

    konu icerisinde cikis islemi yapildi

--- app/src/main/java/mobileprogramming/egitim/Konu.java
+++ app/src/main/java/mobileprogramming/egitim/Konu.java
@@ -13,96 +13,41 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Konu extends AppCompatActivity {
 
-    private Button signOut;
-    private FirebaseAuth auth;
 
+    private FirebaseAuth auth;
+    Button signOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_konu);
-
         auth = FirebaseAuth.getInstance();
 
+         signOut = (Button) findViewById(R.id.sign_out);
 
 
-        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user !=null){
-            Toast.makeText(getApplicationContext(),user.getEmail(),Toast.LENGTH_SHORT).show();
-        }
+        //   final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        //   if (user !=null){
+        //       Toast.makeText(getApplicationContext(),user.getEmail(),Toast.LENGTH_SHORT).show();
+        //   }
 
-        authListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user == null) {
-
-                    startActivity(new Intent(Konu.this, login.class));
-                    finish();
-                }
-            }
-        };
-        signOut = (Button) findViewById(R.id.sign_out);
 
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signOut();
+                auth.signOut();
             }
         });
 
+
     }
 
-    public // this listener will be called when there is change in firebase user session
-    FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
-        @Override
-        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-            FirebaseUser user = firebaseAuth.getCurrentUser();
-            if (user == null) {
-                // user auth state is changed - user is null
-                // launch login activity
-                startActivity(new Intent(Konu.this, login.class));
-                finish();
-            }
-
-
-
-        }
-
-
-    };
-
-    //sign out method
-    public void signOut() {
-        auth.signOut();
-
-
-// this listener will be called when there is change in firebase user session
-        FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user == null) {
-                    // user auth state is changed - user is null
-                    // launch login activity
-                    startActivity(new Intent(Konu.this, login.class));
-                    finish();
-                }
-            }
-        };
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        auth.addAuthStateListener(authListener);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (authListener != null) {
-            auth.removeAuthStateListener(authListener);
-        }
-    }
 }
+
+
+
+
+
+
+
+
