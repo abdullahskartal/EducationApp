2c850d606d7c9ab31b883b2b741f8918b397d73f
saidkartal <as_kartal@hotmail.com>
2018-04-15 16:31:59 +0300

    Tasarım değişiklikleri yapıldı. Kodlar düzenlendi.

--- app/src/main/java/mobileprogramming/egitim/Konu.java
+++ app/src/main/java/mobileprogramming/egitim/Konu.java
@@ -6,22 +6,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Konu extends AppCompatActivity {
 
     private Button signOut;
     private FirebaseAuth auth;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_konu);
-
         auth = FirebaseAuth.getInstance();
 
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -81,6 +85,7 @@ public class Konu extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         auth.addAuthStateListener(authListener);
+
     }
 
     @Override
@@ -90,4 +95,5 @@ public class Konu extends AppCompatActivity {
             auth.removeAuthStateListener(authListener);
         }
     }
+    
 }
